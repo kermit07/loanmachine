6e37da813b6956b59366c9e60eb5d0a48b913e14
kermit07 <abckermit@gmail.com>
2023-10-11 12:17:06 +0200

    initial commit

---
+++ src/main/java/com/test/loanmachine/LoanMachineApplication.java
@@ -0,0 +1,13 @@
+package com.test.loanmachine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoanMachineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoanMachineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/test/loanmachine/LoanMachineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.test.loanmachine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoanMachineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
